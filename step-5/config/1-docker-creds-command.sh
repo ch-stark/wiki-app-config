5d3842e553d9da9bf6375c7a9fd80b299f5d795b
hrishikesh <hrishishinde.p@gmail.com>
2020-01-22 02:56:20 +0530

    updates CD config

--- step-5/config/1-docker-creds-command.sh
+++ step-5/config/1-docker-creds-command.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 kubectl create secret generic docker-creds \
-    --from-file=.dockerconfigjson=~/.docker/config.json \
+    --from-file=.dockerconfigjson=/home/$(whoami)/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson
